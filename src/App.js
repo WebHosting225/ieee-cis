13df7fbcc2eca0d750452b62860fc98cc48103eb
Amith <amitharun3@gmail.com>
2022-09-25 02:14:32 +0530

    skeletal done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import './App.css';
 
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Home from "./pages/home";
-import Gallery from "./pages/gallery";
-import Events from "./pages/events";
-import Committee from "./pages/committee";
 
-export default function () {
-    let links = [
-        {name: "Home", link: "#"},
-        {name: "Gallery", link: "#"},
-        {name: "Events", link: "#"},
-        {name: "Committee", link: "#"}
-    ]
+import {Outlet} from "react-router-dom";
+
+export default function ({links}) {
     return (
-        <div className="App">
+        <>
             <Header imgSrc={"/static/images/ieeeb.png"} links={links}/>
+            <div className="App"><Outlet/></div>
             <Footer imgSrc={"/static/images/ieeeb.png"} links={links}/>
-        </div>
+        </>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,46 @@
 import "./header.css";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 
 export default function ({imgSrc, links}) {
     let header_ref = useRef();
+    const [topPad, setTopPad] = useState(40);
 
-    function overFlowCheck(element, width) {
-        if (window.innerWidth < element.clientWidth && !width) {
-            width = element.clientWidth;
-            element.setAttribute("panned", "out");
-        } else if (window.innerWidth > width) {
-            width = undefined;
+    function overFlowCheck(element) {
+        if (window.innerWidth < element.clientWidth) element.setAttribute("panned", "out");
+        else {
             element.setAttribute("panned", "ok");
+            if (menuToggle[1] === "yes") menuButtonCnt(element);
         }
-        return width
+    }
+
+    function scrollCheck(element) {
+        if (window.scrollY < 25) {
+            setTopPad(40 - window.scrollY);
+            element.setAttribute("scrolled", "false")
+        } else element.setAttribute("scrolled", "true")
+    }
+
+    function menuButtonCnt(element) {
+        element.setAttribute("menu", menuToggle[0]);
+        menuToggle = menuToggle.reverse();
     }
 
     useEffect(() => {
-        let element = header_ref.current, width = overFlowCheck(element);
-        window.addEventListener('resize', () => width = overFlowCheck(element, width));
+        overFlowCheck(header_ref.current);
+        window.addEventListener('resize', () => overFlowCheck(header_ref.current));
+        window.addEventListener('scroll', () => scrollCheck(header_ref.current));
     }, [])
     let menuToggle = ["yes", "no"];
     return (
-        <header ref={header_ref}>
+        <header ref={header_ref} style={{paddingTop: topPad}}>
             <img src={imgSrc} className="header-hero" alt="header hero"/>
             <div className="header-pan">
                 <ul className="header-links">
-                    {links.map(link => <li key={links.indexOf(link)}><a href={link.ref}>{link.name}</a></li>)}
+                    {links.map(link => <li key={links.indexOf(link)}>{link.link}</li>)}
                 </ul>
-                <button onClick={() => {
-                    header_ref.current.setAttribute("menu", menuToggle[0]);
-                    menuToggle = menuToggle.reverse();
-                }} className="header-links-menu">|||
+                <button onClick={() => menuButtonCnt(header_ref.current)} className="header-links-menu">
+                    |||
                 </button>
             </div>
         </header>
